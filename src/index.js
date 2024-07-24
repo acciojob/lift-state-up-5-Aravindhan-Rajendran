2ddd1ed8510a0474173fcb4963bdc301c460ca66
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-24 21:44:06 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {login} from "./components.App.js";
-<h1>Parent Component</h1>
-<form>
-  <label for="username">Username:</label>
-  <input type="text" id="username">
-  <label for="password">Password:</label>
-  <input type="password" id="password">
-  <button type="button" id="login">Login</button>
-  </form>
+// src/LoginForm.js
+import React from 'react';
+
+function LoginForm({ onLogin }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onLogin();
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
