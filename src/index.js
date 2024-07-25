9ab74c70a5b803b3b4db55f4347acb0d19d13fd6
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-25 21:14:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'; // Ensure this path is correct
-import '.src/styles/App.css'; // Ensure this path is correct
+import './styles/App.css'; // Ensure this path is correct
 
 ReactDOM.render(<App />, document.getElementById('root'));
